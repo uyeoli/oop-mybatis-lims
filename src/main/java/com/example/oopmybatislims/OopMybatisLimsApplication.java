2e7f2b52ce1fb92c16638a7afeb68fd5a4ece861
uyeoli <uyeol134@naver.com>
2025-03-21 00:41:10 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/oopmybatislims/OopMybatisLimsApplication.java
@@ -0,0 +1,13 @@
+package com.example.oopmybatislims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopMybatisLimsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopMybatisLimsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oopmybatislims/OopMybatisLimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oopmybatislims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopMybatisLimsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
